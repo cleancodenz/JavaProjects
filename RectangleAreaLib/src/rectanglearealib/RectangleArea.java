4002f3cb76327a6ed9f401b010d6aaa170df18a3
johnson <johnson@johnson-VirtualBox>
2016-01-03 16:18:13 +1300

    Project setup

---
+++ RectangleAreaLib/src/rectanglearealib/RectangleArea.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rectanglearealib;
+
+/**
+ *
+ * @author johnson
+ */
+public final class RectangleArea {
+
+     public static int area(java.awt.Rectangle r) {
+        return r.width * r.height;
+    }
+    
+}
