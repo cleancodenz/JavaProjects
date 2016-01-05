c33ee20b283bab9bd8e494bb5255d123ed9d312f
Johnson <johnsonwangnz@gmail.com>
2016-01-05 15:15:42 +1300

    DictionaryDemo changes

--- DictionaryDemo/src/dictionary/DictionaryDemo.java
+++ DictionaryDemo/src/dictionary/DictionaryDemo.java
@@ -15,7 +15,22 @@ public class DictionaryDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       DictionaryService dictionary = DictionaryService.getInstance();
+        System.out.println(DictionaryDemo.lookup(dictionary, "book"));
+        System.out.println(DictionaryDemo.lookup(dictionary, "editor"));
+        System.out.println(DictionaryDemo.lookup(dictionary, "xml"));
+        System.out.println(DictionaryDemo.lookup(dictionary, "REST"));
+        
     }
+    
+     public static String lookup(DictionaryService dictionary, String word) {
+    String outputString = word + ": ";
+    String definition = dictionary.getDefinition(word);
+    if (definition == null) {
+      return outputString + "Cannot find definition for this word.";
+    } else {
+      return outputString + definition;
+    }
+  }
     
 }
